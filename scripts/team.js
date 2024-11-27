3d2dc2252d2520f80e53da220420ac5628013bc4
alelin <alexanderbdjgw@gmail.com>
2024-11-27 14:37:48 +0100

    Added values for dimitar and mustafa

--- scripts/team.js
+++ scripts/team.js
@@ -10,11 +10,9 @@ function Member(firstName, lastName, age, location, hobby) {
 }
 
 let alex = new Member("Alexander", "Lindström", 31, "Sickla", "Japanese")
-let dimitar = new Member("Dimitar", "Placeholder", 1111, "Placeholder", "Placeholder")
-let mustafa = new Member("Mustafa", "Placeholder", 1111, "Placeholder", "Placeholder")
+let dimitar = new Member("Dimitar", "Manastirski", 21, "Bromma", "Workout")
+let mustafa = new Member("Mustafa", "Larhnimi", 34, "Täby", "Guitar")
 let andre = new Member("André", "Placeholder", 1111, "Placeholder", "Placeholder")
 let adam = new Member("Adam", "Placeholder", 1111, "Placeholder", "Placeholder")
 
 let team = [alex, dimitar, mustafa, andre, adam]
-
-console.log(alex.fullName)
