2a7e06cd43e4c2917d16a0d0528f01c0bd3dd2ce
andre84o <andre.torabpour@gmail.com>
2024-11-28 17:20:59 +0100

    fixed sidebar

---
+++ scripts/reptiles.js
@@ -0,0 +1,21 @@
+let frillNeckedLizard = {
+    name: "Frill-necked lizard",
+    lifespan: "20 years",
+    group: "Reptile",
+    food: "Small insects and spiders"
+};
+
+let HawksbillTurtle =  {
+    lifespan: "50 years",
+    group: "reptile ",
+    food: "Other animals (sponges & jellyfish), sea plants"
+};
+
+let Perentie = {
+    lifespan: "20 years",
+    group: "reptile",
+    food: "Carnivore, they eat animals like kangaroos, rabbits, lizards and birds"
+};
+
+
+
