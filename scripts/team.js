7a6df9ffa922f2e6b0e327d99956a3e69bf62c9e
alelin <alexanderbdjgw@gmail.com>
2024-11-29 16:39:41 +0100

    Added Javascript for sidebar in teams

--- scripts/team.js
+++ scripts/team.js
@@ -16,3 +16,29 @@ let andre = new Member("André", "Torabpour", 40, "Råcksta", "Guitar")
 let adam = new Member("Adam", "Kaya", 34, "Sätra", "Music")
 
 let team = [alex, dimitar, mustafa, andre, adam]
+
+const constructSidebarList = () => {
+    let list = document.querySelector(".sidebar__list")
+    team.forEach(member => {
+        list.innerHTML += ` <li>
+                                <a href="#">
+                                    <img src="../icons/echidna.png" alt="${member.firstName.toLowerCase()} Icon" class="icon-side ${member.firstName.toLowerCase()}-icon">
+                                    <span class="nav-item">${member.firstName}</span>
+                                </a>
+                            <span class="tooltip">${member.firstName}</span>
+                            </li>`
+    });
+    
+}
+
+constructSidebarList();
+
+
+
+/*<li>
+<a href="#">
+    <img src="../icons/echidna.png" alt="echidna Icon" class="icon-side echidna-icon">
+    <span class="nav-item">Echidna</span>
+</a>
+<span class="tooltip">Echidna</span>
+</li>*/
