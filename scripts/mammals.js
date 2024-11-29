b5fe978c0f3b372451517a8c0a3b41b14af60547
AwdamnKaya <awdamnkaya@users.noreply.github.com>
2024-11-29 13:49:29 +0100

    added function

--- scripts/mammals.js
+++ scripts/mammals.js
@@ -1,1 +1,12 @@
-function Animal()
+function Animal(name, lifespan, group, food, description, length, weight, found) {
+    this.name = name;
+    this.lifespan = lifespan;
+    this.group = group;
+    this.food = food;
+    this.description = description;
+    this.length = length;
+    this.weight = weight;
+    this.found = found;
+}
+
+
