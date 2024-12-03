3ceea5df5116285b05e056e015b63f9e8309eb3e
Dimitar Manastirsky <dmanastirski@gmail.com>
2024-12-03 19:23:00 +0100

    made header and sidebar fixed position and added display

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,7 +1,9 @@
 let btn = document.querySelector('#btn');
 let sidebar = document.querySelector('.sidebar');
+let main_content = document.querySelector('.main-content');
 
 btn.addEventListener('click', function () {
     sidebar.classList.toggle('active');
+    main_content.classList.toggle('active');
 });
 
