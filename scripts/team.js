ae8ac27d56362f53796177555833c06c30f59daa
alelin <alexanderbdjgw@gmail.com>
2024-12-04 13:02:03 +0100

    Added some margin

--- scripts/team.js
+++ scripts/team.js
@@ -34,24 +34,22 @@ constructSidebarList();
 let buttons = Array.from(document.querySelectorAll(".team-sidebar-button"))
 let content = document.querySelector(".team-content")
 
-//const selectTeamMember = () => {
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            let teamMember = team[buttons.indexOf(button)]
-            let teamMemberName = teamMember.firstName.toLowerCase();
-            if (button.classList.contains(teamMemberName)) {
-                content.innerHTML = `<div class="team-content__container">
-                                        <img src="../image/${teamMemberName}.png" alt="picture of ${teamMemberName}" width="25%" class="team-content__image">
-                                            <div class="team-content__text-container">
-                                                <h3 class="team-content__title">${teamMember.fullName}</h3>
-                                                <p class="team-content__text-paragraph">${teamMember.age} years old.</p>
-                                                <p class="team-content__text-paragraph">Lives in ${teamMember.location}.</p>
-                                                <p class="team-content__text-paragraph">Has ${teamMember.hobby.toLowerCase()} as a hobby.</p>
-                                            </div>
-                                        </div>`
-                buttons.forEach(otherButton => {otherButton.classList.remove("selected")})
-                button.classList.add("selected")
-            }
-        })
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        let teamMember = team[buttons.indexOf(button)]
+        let teamMemberName = teamMember.firstName.toLowerCase();
+        if (button.classList.contains(teamMemberName)) {
+            content.innerHTML = `<div class="team-content__container">
+                                    <img src="../image/${teamMemberName}.png" alt="picture of ${teamMemberName}" width="25%" class="team-content__image">
+                                        <div class="team-content__text-container">
+                                            <h3 class="team-content__title">${teamMember.fullName}</h3>
+                                            <p class="team-content__text-paragraph">${teamMember.age} years old.</p>
+                                            <p class="team-content__text-paragraph">Lives in ${teamMember.location}.</p>
+                                            <p class="team-content__text-paragraph">Has ${teamMember.hobby.toLowerCase()} as a hobby.</p>
+                                        </div>
+                                    </div>`
+            buttons.forEach(otherButton => {otherButton.classList.remove("selected")})
+            button.classList.add("selected")
+        }
     })
-//}
+})
