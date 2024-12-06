4f9e7b0c51a479ef0f9c42b9eb12df95f1716b31
AwdamnKaya <awdamnkaya@users.noreply.github.com>
2024-12-06 15:11:50 +0100

    created objects integrated them into sidebar

--- scripts/mammals.js
+++ scripts/mammals.js
@@ -9,7 +9,7 @@ function Animal(name, lifespan, group, food, description, length, weight, found)
     this.found = found;
 }
 
-let animals = [
+let mammals = [
     new Animal(
         "Echidna",
         50,
@@ -25,7 +25,7 @@ let animals = [
         "Tasmanian Devil",
         5,
         "Mammals",
-        "A predator, then eat meat from other animals such as wallabies and wombats",
+        "A predator, then eat meat from other mammals such as wallabies and wombats",
         "The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina)[3] is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.",
         70,
         10,
@@ -47,7 +47,7 @@ let animals = [
 let initialShowcaseHTML = `
         <div class="title">Welcome!</div>
         <div class="welcome-description">This is our animal Zoo. WOW</div>
-        <div class="pet-house"><img src="./icons/pet-house.png" alt=""></div>
+        <div class="pet-house"><img src="../icons/pet-house.png" alt=""></div>
     `
 
 let showcase = document.querySelector(".showcase");
@@ -55,14 +55,14 @@ showcase.innerHTML = initialShowcaseHTML;
 
 let sidebarHTML = document.querySelector(".sidebar ul");
 
-animals.forEach(animal => {
+mammals.forEach(mammal => {
     sidebarHTML.innerHTML += `
             <li>
                 <a href="#" class="animal">
-                    <img src="./icons/${animal.name.toLowerCase()}.png" alt="${animal.name.toLowerCase()} Icon" class="icon-side ${animal.name.toLowerCase()}-icon">
-                    <span class="nav-item">${animal.name}</span>
+                    <img src="../icons/${mammal.name.toLowerCase()}.png" alt="${mammal.name.toLowerCase()} Icon" class="icon-side ${mammal.name.toLowerCase()}-icon">
+                    <span class="nav-item">${mammal.name}</span>
                 </a>
-                <span class="tooltip">${animal.name}</span>
+                <span class="tooltip">${mammal.name}</span>
             </li>
     `
 });
@@ -79,7 +79,7 @@ animalButtons.forEach(b => b.addEventListener('click', ()=> {
         } 
         else b.classList.add("animal-active");
 
-        let selectedAnimal = animals.find(a => a.name === b.querySelector(".nav-item").innerHTML);
+        let selectedAnimal = mammals.find(a => a.name === b.querySelector(".nav-item").innerHTML);
 
         showcase.innerHTML = `
         <div class="title">${selectedAnimal.name}</div>
@@ -123,7 +123,7 @@ animalButtons.forEach(b => b.addEventListener('click', ()=> {
             </div>
         </div>
 
-        <div class="pet-house"><img src="./icons/pet-house.png" alt=""></div>
+        <div class="pet-house"><img src="../icons/pet-house.png" alt=""></div>
         `
     }
     else {
