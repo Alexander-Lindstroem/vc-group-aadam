605bf8732d39bdc07773fc8edfc00080a5a8a241
Dimitar Manastirsky <dmanastirski@gmail.com>
2024-12-09 04:58:17 +0100

    made develop organized with an idea how things must be going

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,19 +11,19 @@ window.addEventListener('resize', () => {
     if (window.innerWidth < 605) {
         list.innerHTML = `
             <li class="next-button">
-                <a href="index.html"><img src="./menu-icons/home-icon.png" alt=""></a>
+                <a href="index.html"><img src="../menu-icons/home-icon.png" alt=""></a>
             </li>
             <li class="next-button">
-                <a href="./html/mammals.html"><img src="./menu-icons/mammals-icon.png" alt=""></a>
+                <a href="../html/mammals.html"><img src="../menu-icons/mammals-icon.png" alt=""></a>
             </li>
             <li class="next-button">
-                <a href="./html/reptiles.html"><img src="./menu-icons/reptiles-icon.png" alt=""></a>
+                <a href="../html/reptiles.html"><img src="../menu-icons/reptiles-icon.png" alt=""></a>
             </li>
             <li class="next-button">
-                <a href="./html/birds.html"><img src="./menu-icons/birds-icon.png" alt=""></a>
+                <a href="../html/birds.html"><img src="../menu-icons/birds-icon.png" alt=""></a>
             </li>
             <li>
-                <a href="./html/team.html"><img src="./menu-icons/about-us-icon.png" alt=""></a>
+                <a href="../html/team.html"><img src="../menu-icons/about-us-icon.png" alt=""></a>
             </li>
         `
     }
