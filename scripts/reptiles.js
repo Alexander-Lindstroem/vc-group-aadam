7d0027943ac09847da8f2aae575a0d2560b1a4ba
alelin <alexanderbdjgw@gmail.com>
2025-01-07 11:43:31 +0100

    fixed icons not displaying on reptile page

--- scripts/reptiles.js
+++ scripts/reptiles.js
@@ -1,4 +1,4 @@
-function Animal(name, lifespan, group, food, description, length, weight, found) {
+function Animal(name, lifespan, group, food, description, length, weight, found, filename) {
     this.name = name;
     this.lifespan = lifespan;
     this.group = group;
@@ -7,6 +7,7 @@ function Animal(name, lifespan, group, food, description, length, weight, found)
     this.length = length;
     this.weight = weight;
     this.found = found;
+    this.filename = filename;
 }
 
 let reptiles = [
@@ -18,18 +19,20 @@ frillNeckedLizard = new Animal(
     "When this unique creature feels threatened, it rises on its hind legs, opens its yellow-coloured mouth, unfurls the colorful, pleated skin flap that encircles its head, and hisses. If an attacker is unintimidated by these antics, the lizard simply turns tail, mouth and frill open, and bolts, legs splaying left and right. It continues its deliberate run without stopping or looking back until it reaches the safety of a tree.", 
     "90cm", 
     "1 kg", 
-    "Northern Australia"
+    "Northern Australia",
+    "frill-necked-lizard"
 ),
 
 hawksbillTurtle = new Animal(
-    "Hawkbill turtle",
+    "Hawksbill turtle",
     "50 years ", 
     "Reptile", 
     "other animals (sponges & jellyfish), sea plants", 
     "The Hawksbill Sea Turtle gets its common name from the distinctive mouth, which resembles a bird’s beak. The shell, or carapace, is covered in large overlapping scutes, which are a distinctive brown/green/amber background with lighter brown streaks throughout. At the edge of the carapace they overlap in such a way as to form a serrated edge. The turtle has an elongated head and flippers which have two visible claws on the end. The flippers and head are covered in large green, brown or yellow scales. The average adult female weighs 50 kg and their carapace (shell) is approximately 80 cm in length.",
     "80cm (carapace)", 
     "50kg",
-    "Tropical coasts of Queensland, Northern Territory and Western Australia."
+    "Tropical coasts of Queensland, Northern Territory and Western Australia.",
+    "hawksbill-turtle"
 ),
 
 perentie  = new Animal(
@@ -40,7 +43,8 @@ perentie  = new Animal(
     "The perentie (Varanus giganteus) is the largest monitor lizard or goanna native to Australia. It is one of the largest living lizards on earth, after the Komodo dragon, Asian water monitor, crocodile monitor, and intersecting by size with Nile monitor.[3] Found west of the Great Dividing Range in the arid areas of Australia, it is rarely seen, because of its shyness and the remoteness of much of its range from human habitation. The species is considered to be a least-concern species according to the International Union for Conservation of Nature. Its status in many Aboriginal cultures is evident in the totemic relationships, and part of the Ngiṉṯaka dreaming, as well as bush tucker. It was a favoured food item among desert Aboriginal tribes, and the fat was used for medicinal and ceremonial purposes.", 
     "2.5 m", 
     "20kg", 
-    "Deserts"
+    "Deserts",
+    "perentie"
 ),
 ]
 
@@ -53,7 +57,7 @@ const constructSidebarList = () => {
     reptiles.forEach(animal => {
         let animalNameButton = document.createElement("li");
         animalNameButton.innerHTML =  `<a href="#" class="nav-link" data-name="${animal.name.toLowerCase()}">
-            <img src="../icons/${animal.name.toLowerCase()}.png" alt="Frill-necked Lizard Icon" class="icon-side frill-necked-lizard-icon">
+            <img src="../icons/${animal.filename}.png" alt="Frill-necked Lizard Icon" class="icon-side frill-necked-lizard-icon">
             <span class="nav-item" id="frill-necked-lizard">${animal.name}</span>
         </a>
         <span class="tooltip lizard">${animal.name}</span>`
